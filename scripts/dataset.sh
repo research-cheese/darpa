65612a797514e59c1436a5c4441776dd734f5e33
hwelsters <hwelsters>
2024-09-07 16:47:30 -0700

    Changes

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,21 +1,21 @@
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "base" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "test" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "val" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "dust-10" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "dust-100" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "dust-1000" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "fog-10" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "fog-100" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "fog-1000" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "maple_leaf-10" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "maple_leaf-100" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "maple_leaf-1000" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "rain-10" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "rain-100" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "rain-1000" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "snow-10" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "snow-100" &
-python3 "packages/1. dataset/src/process_dataset.py" --dataset_name "snow-1000" &
+python3 "packages/1. dataset/src/process_dataset.py" "base" &
+python3 "packages/1. dataset/src/process_dataset.py" "test" &
+python3 "packages/1. dataset/src/process_dataset.py" "val" &
+python3 "packages/1. dataset/src/process_dataset.py" "dust-10" &
+python3 "packages/1. dataset/src/process_dataset.py" "dust-100" &
+python3 "packages/1. dataset/src/process_dataset.py" "dust-1000" &
+python3 "packages/1. dataset/src/process_dataset.py" "fog-10" &
+python3 "packages/1. dataset/src/process_dataset.py" "fog-100" &
+python3 "packages/1. dataset/src/process_dataset.py" "fog-1000" &
+python3 "packages/1. dataset/src/process_dataset.py" "maple_leaf-10" &
+python3 "packages/1. dataset/src/process_dataset.py" "maple_leaf-100" &
+python3 "packages/1. dataset/src/process_dataset.py" "maple_leaf-1000" &
+python3 "packages/1. dataset/src/process_dataset.py" "rain-10" &
+python3 "packages/1. dataset/src/process_dataset.py" "rain-100" &
+python3 "packages/1. dataset/src/process_dataset.py" "rain-1000" &
+python3 "packages/1. dataset/src/process_dataset.py" "snow-10" &
+python3 "packages/1. dataset/src/process_dataset.py" "snow-100" &
+python3 "packages/1. dataset/src/process_dataset.py" "snow-1000" &
 
 
 python3 "packages/1. dataset/src/convert_to_cat.py"
