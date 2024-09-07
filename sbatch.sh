6e2f93c60d6f1b6960e4b09bfe1b83e4fc45788c
hwelsters <hwelsters>
2024-09-07 13:19:33 -0700

    Changes

---
+++ sbatch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -N 1            # number of nodes
+#SBATCH -c 8            # number of cores 
+#SBATCH -t 0-01:00:00   # time in d-hh:mm:ss
+#SBATCH -p general      # partition 
+#SBATCH -q public       # QOS
+#SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
+#SBATCH --mail-user="%u@asu.edu"
+#SBATCH --export=NONE   # Purge the job-submitting shell environment
+
+
+#Load CuDA
+module load cuda-12.6.1-gcc-12.1.0
+
+source a.sh
