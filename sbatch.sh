df7ee2404eef85d45b1e965fa6d99ae38db95313
hwelsters <hwelsters>
2024-09-07 16:52:27 -0700

    Changes

--- sbatch.sh
+++ sbatch.sh
@@ -2,8 +2,8 @@
 #SBATCH -N 1            # number of nodes
 #SBATCH -c 8            # number of cores 
 #SBATCH -t 0-12:00:00   # time in d-hh:mm:ss
-#SBATCH -p highmem      # partition 
-#SBATCH -q grp_scai_research_priority       # QOS
+#SBATCH -p highmem/grp_scai_research_priority      # partition 
+#SBATCH -q public       # QOS
 #SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
 #SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
 #SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
