33f7e906cee7e808883c545438acb6398ed1c58e
hwelsters <hwelsters>
2024-09-07 17:03:19 -0700

    Changes

--- sbatch.sh
+++ sbatch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH -N 1            # number of nodes
 #SBATCH -c 8            # number of cores 
-#SBATCH -t 0-12:00:00   # time in d-hh:mm:ss
+#SBATCH -G 1            # number of cores 
+#SBATCH -t 1-00:00:00   # time in d-hh:mm:ss
 #SBATCH -p highmem      # partition 
-#SBATCH -q grp_scai_research_priority # QOS
-#SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
-#SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH -q grp_scai_research_priority
+#SBATCH -o slurms/slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e slurms/slurm.%j.err # file to save job's STDERR (%j = JobId)
 #SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
 #SBATCH --mail-user="%nngu2@asu.edu"
 #SBATCH --export=NONE   # Purge the job-submitting shell environment
