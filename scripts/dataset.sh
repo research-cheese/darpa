953d98fa735644f172c7348f50894cc5beec27b3
hwelsters <hwelsters>
2024-09-07 22:05:13 -0700

    Changes

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,3 +1,15 @@
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 0 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 1000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 2000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 3000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 4000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 5000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 6000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 7000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 8000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 9000 &
+
+
 python3 "packages/1. dataset/src/process_dataset.py" "base" &
 python3 "packages/1. dataset/src/process_dataset.py" "test" &
 python3 "packages/1. dataset/src/process_dataset.py" "val" &
