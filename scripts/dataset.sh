6025fcc6d969d7aca33eb2d2ee67526cb107fe21
hwelsters <hwelsters>
2024-09-07 17:34:11 -0700

    Changes

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,21 +1,21 @@
-python3 "packages/1. dataset/src/process_dataset.py" "base"
-python3 "packages/1. dataset/src/process_dataset.py" "test"
-python3 "packages/1. dataset/src/process_dataset.py" "val"
-python3 "packages/1. dataset/src/process_dataset.py" "dust-10"
-python3 "packages/1. dataset/src/process_dataset.py" "dust-100"
-python3 "packages/1. dataset/src/process_dataset.py" "dust-1000"
-python3 "packages/1. dataset/src/process_dataset.py" "fog-10"
-python3 "packages/1. dataset/src/process_dataset.py" "fog-100"
-python3 "packages/1. dataset/src/process_dataset.py" "fog-1000"
-python3 "packages/1. dataset/src/process_dataset.py" "maple_leaf-10"
-python3 "packages/1. dataset/src/process_dataset.py" "maple_leaf-100"
-python3 "packages/1. dataset/src/process_dataset.py" "maple_leaf-1000"
-python3 "packages/1. dataset/src/process_dataset.py" "rain-10"
-python3 "packages/1. dataset/src/process_dataset.py" "rain-100"
-python3 "packages/1. dataset/src/process_dataset.py" "rain-1000"
-python3 "packages/1. dataset/src/process_dataset.py" "snow-10"
-python3 "packages/1. dataset/src/process_dataset.py" "snow-100"
-python3 "packages/1. dataset/src/process_dataset.py" "snow-1000"
+python3 "packages/1. dataset/src/process_dataset.py" "base" &
+python3 "packages/1. dataset/src/process_dataset.py" "test" &
+python3 "packages/1. dataset/src/process_dataset.py" "val" &
+python3 "packages/1. dataset/src/process_dataset.py" "dust-10" &
+python3 "packages/1. dataset/src/process_dataset.py" "dust-100" &
+python3 "packages/1. dataset/src/process_dataset.py" "dust-1000" &
+python3 "packages/1. dataset/src/process_dataset.py" "fog-10" &
+python3 "packages/1. dataset/src/process_dataset.py" "fog-100" &
+python3 "packages/1. dataset/src/process_dataset.py" "fog-1000" &
+python3 "packages/1. dataset/src/process_dataset.py" "maple_leaf-10" &
+python3 "packages/1. dataset/src/process_dataset.py" "maple_leaf-100" &
+python3 "packages/1. dataset/src/process_dataset.py" "maple_leaf-1000" &
+python3 "packages/1. dataset/src/process_dataset.py" "rain-10" &
+python3 "packages/1. dataset/src/process_dataset.py" "rain-100" &
+python3 "packages/1. dataset/src/process_dataset.py" "rain-1000" &
+python3 "packages/1. dataset/src/process_dataset.py" "snow-10" &
+python3 "packages/1. dataset/src/process_dataset.py" "snow-100" &
+python3 "packages/1. dataset/src/process_dataset.py" "snow-1000" 
 
 
 python3 "packages/1. dataset/src/convert_to_cat.py"
