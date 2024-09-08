2e0482ed2c62be8863276def2d78a8bb739c1a9d
hwelsters <hwelsters>
2024-09-07 22:12:53 -0700

    Changes

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,15 +1,27 @@
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 0 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 1000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 1500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 2000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 2500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 3000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 3500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 4000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 4500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 5000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 5500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 6000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 6500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 7000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 7500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 8000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 8500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 9000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 9500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 10000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 10500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 11000 &
+python3 "packages/1. dataset/src/dumb_thread.py" "base" 11500 &
 python3 "packages/1. dataset/src/dumb_thread.py" "base" 12000 
 
 
