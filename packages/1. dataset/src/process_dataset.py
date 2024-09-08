6b88bb81f2b144a232ce8c92b6d4b6817512cc7e
hwelsters <hwelsters>
2024-09-07 23:55:19 -0700

    Changes

--- packages/1. dataset/src/process_dataset.py
+++ packages/1. dataset/src/process_dataset.py
@@ -24,7 +24,6 @@ def process_dataset(dataset_path):
         saved_bounding_box_path = f"{sample_path}/bounding_box.jsonl"
 
         if os.path.exists(ground_truth_path) and os.path.exists(saved_bounding_box_path): continue
-
         if os.path.exists(saved_bounding_box_path): os.remove(saved_bounding_box_path)
         if os.path.exists(ground_truth_path): shutil.rmtree(ground_truth_path)
         os.makedirs(ground_truth_path)
@@ -34,6 +33,7 @@ def process_dataset(dataset_path):
             (VEHICLE_COLORS, "vehicle", False, 30), 
             (CONSTRUCTION_COLORS, "construction", True, 30), 
             (NATURE_COLORS, "nature", False, 50)]:
+
             truth = get_ground_truth(segmentation_path, colors, group_disjoint=group_disjoint, min_count=min_count)
 
             index = 0
@@ -47,6 +47,7 @@ def process_dataset(dataset_path):
                 with open(saved_bounding_box_path, "a") as f:
                     f.write(json.dumps({"class": color_name, **t.bounding_box.__dict__}))
                     f.write("\n")
+                    print({"class": color_name, **t.bounding_box.__dict__})
 
                 index += 1
 
